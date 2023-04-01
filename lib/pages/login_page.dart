889f68b495daa8145a8acfc6017d89e94aeb5dcf
Amritpal Singh <er.amritpal17@gmail.com>
2023-04-01 18:30:52 +0530

    Setup

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //logo
+
+          //welcome back, you've been missed
+
+          //usernamd textfield
+
+          //forgot password
+
+          //sign up button
+
+          //or continuje with
+
+          // google apple sign in button
+
+          //not a member register now
+        ],
+      ),
+    );
+  }
+}
