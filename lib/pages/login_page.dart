d5d0a157eaaa18eeea5c51159ff51173d33debe8
Amritpal Singh <er.amritpal17@gmail.com>
2023-04-01 18:37:39 +0530

    Adding logo

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,24 +7,28 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          //logo
+      body: SafeArea(
+        child: Column(
+          children: [
+            //logo
+            Icon(Icons.lock,
+            size: 100,)
 
-          //welcome back, you've been missed
+            //welcome back, you've been missed
 
-          //usernamd textfield
+            //usernamd textfield
 
-          //forgot password
+            //forgot password
 
-          //sign up button
+            //sign up button
 
-          //or continuje with
+            //or continuje with
 
-          // google apple sign in button
+            // google apple sign in button
 
-          //not a member register now
-        ],
+            //not a member register now
+          ],
+        ),
       ),
     );
   }
