2bc5e94ba0f559614968ca4c45f3ae985d4de60c
Amritpal Singh <er.amritpal17@gmail.com>
2023-04-02 14:33:16 +0530

    Adding username and password textfields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:login_ui/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,48 +16,50 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              const SizedBox(height: 50,),
+              const SizedBox(
+                height: 50,
+              ),
 
               //logo
-              const Icon(Icons.lock,
-              size: 100,
+              const Icon(
+                Icons.lock,
+                size: 100,
               ),
 
               const SizedBox(height: 50),
 
               //welcome back, you've been missed
               Text(
-                  'Welcome back you\'ve been missed!',
-                style: TextStyle(color: Colors.grey[700],
-                fontSize: 16,
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
               ),
 
               const SizedBox(height: 25),
 
-              //usernamd textfield
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey.shade400),
-                  ),
-                    fillColor: Colors.grey.shade200,
-                      filled: true,
-                    ),
-                  ),
+              //username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: true,
               ),
 
+              const SizedBox(height: 25),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
 
               //forgot password
 
               //sign up button
 
-              //or continuje with
+              //or continue with
 
               // google apple sign in button
 
