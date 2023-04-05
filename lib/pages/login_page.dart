626f4171696cce4b8bdd81df3ea86c2c877bc820
Amritpal Singh <er.amritpal17@gmail.com>
2023-04-05 20:20:57 +0530

    adding sign buttons

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:login_ui/components/my_button.dart';
 import 'package:login_ui/components/my_textfield.dart';
+import 'package:login_ui/components/square_tile.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -8,6 +10,9 @@ class LoginPage extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  //sign user in method
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +20,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
                 height: 50,
@@ -55,15 +61,94 @@ class LoginPage extends StatelessWidget {
                 obscureText: true,
               ),
 
+              const SizedBox(height: 10),
+
               //forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
 
               //sign up button
 
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50),
+
               //or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or Continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                        child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ))
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
 
               // google apple sign in button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  //google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  //apple button
+                  SquareTile(imagePath: 'lib/images/apple.png')
+                ],
+              ),
+              const SizedBox(height: 50),
 
               //not a member register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
             ],
           ),
         ),
